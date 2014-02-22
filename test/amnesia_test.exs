f6b9baa1846526488212a1ffb35cb570d4f74652
Peter Brändel <peter@bitbites.de>
2014-02-22 17:12:26 +0100

    Initial

---
+++ test/amnesia_test.exs
@@ -0,0 +1,66 @@
+Code.require_file "../test_helper.exs", __ENV__.file
+
+use Amnesia
+
+defdatabase Dwitter.Database do
+  deftable Dweet, [:id, :content, :in_reply_to_id], type: :ordered_set do
+    @type t :: Dweet[id: integer, content: String.t, in_reply_to_id: integer]
+
+    def in_reply_to self do
+      Dweet.read self.in_reply_to_id
+    end
+
+    def replies self do
+      Dweet.where(in_reply_to_id == self.id).values
+    end
+  end
+end
+
+defmodule AmnesiaTest do
+  use ExUnit.Case
+  use Dwitter.Database
+
+  test "saving dweets" do
+    Amnesia.transaction! do
+      dweet = Dweet[id: 1, content: "some content"]
+      dweet.write
+    end
+
+    assert "some content" == Dweet.read!(1).content
+  end
+
+  test "finding replies" do
+    Amnesia.transaction! do
+      dweet = Dweet[id: 1, content: "some content"]
+      dweet.write
+      reply = Dweet[id: 2, content: "tell me more content", in_reply_to_id: 1]
+      reply.write
+    end
+
+    Amnesia.transaction! do
+      assert "some content" == Dweet.read!(2).in_reply_to.content
+      [reply] = Dweet.read!(1).replies
+      assert "tell me more content" == reply.content
+    end
+  end
+
+  setup_all do
+    Amnesia.Test.start
+  end
+
+  teardown_all do
+    Amnesia.Test.stop
+  end
+
+  setup do
+    Dwitter.Database.create!
+
+    :ok
+  end
+
+  def teardown do
+    Dwitter.Database.destroy
+
+    :ok
+  end
+end
---
+++ test/test_helper.exs
@@ -0,0 +1,33 @@
+Dynamo.under_test(Dwitter.Dynamo)
+Dynamo.Loader.enable
+ExUnit.start
+
+defmodule Amnesia.Test do
+  def start do
+    :error_logger.tty false
+
+    Amnesia.Schema.create
+    Amnesia.start
+
+    :ok
+  end
+
+  def stop do
+    Amnesia.stop
+    Amnesia.Schema.destroy
+
+    :error_logger.tty true
+
+    :ok
+  end
+end
+
+defmodule Dwitter.TestCase do
+  use ExUnit.CaseTemplate
+
+  # Enable code reloading on test cases
+  setup do
+    Dynamo.Loader.enable
+    :ok
+  end
+end
